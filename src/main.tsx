f1708a2b959ea209d490d7d586ed37da5b834edb
rightware-corporations <rightware123@gmail.com>
2026-08-28 18:54:11 +0200

    feat(public): load public page v2 styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from './app/App'
 import { AppProviders } from './app/providers/AppProviders'
 import './styles/global.css'
 import './styles/public-v2.css'
+import './styles/public-pages-v2.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
