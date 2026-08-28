60941d0c7839b9ba9e27345afd1c8e4c0d522b60
rightware-corporations <rightware123@gmail.com>
2026-08-28 18:50:55 +0200

    feat(public): load Castro's public v2 styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { App } from './app/App'
 import { AppProviders } from './app/providers/AppProviders'
 import './styles/global.css'
+import './styles/public-v2.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
