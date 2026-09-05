1b07f6efe01d4747a623cf62a11e75e2a376e293
rightware-corporations <rightware123@gmail.com>
2026-09-05 18:52:32 +0200

    fix(frontend): make motion features deterministic

--- frontend/src/design-system/motion/MotionProvider.tsx
+++ frontend/src/design-system/motion/MotionProvider.tsx
@@ -1,8 +1,6 @@
-import { LazyMotion, MotionConfig } from 'motion/react'
+import { domAnimation, LazyMotion, MotionConfig } from 'motion/react'
 import type { PropsWithChildren } from 'react'
 
-const loadMotionFeatures = () => import('./motionFeatures').then((module) => module.default)
-
 export function MotionProvider({ children }: PropsWithChildren) {
-  return <MotionConfig reducedMotion="user"><LazyMotion features={loadMotionFeatures} strict>{children}</LazyMotion></MotionConfig>
+  return <MotionConfig reducedMotion="user"><LazyMotion features={domAnimation} strict>{children}</LazyMotion></MotionConfig>
 }
